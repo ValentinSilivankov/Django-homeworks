0880ebd4523ac741bc352d8fb546b3d948ea850b
Valentin_Silivankou <litin20011995@gmail.com>
2023-05-14 09:27:07 +0300

    Edited task 2.1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,18 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+    price = models.IntegerField()
+    image = models.ImageField()
+    release_date = models.DateField(auto_now_add=False)
+    lte_exists = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self,  *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super(Phone, self).save(*args, **kwargs)
