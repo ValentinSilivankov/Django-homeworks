bd54fcd01ee00c6761f29df4c6246823a4ffa6b2
Valentin_Silivankou <litin20011995@gmail.com>
2023-06-07 16:27:27 +0300

    Edited task_2.2_

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -73,9 +73,11 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_orm_migrations',
+        'NAME': 'Name_DB',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'your_password'
     }
 }
 
@@ -130,4 +132,3 @@ try:
     from .settings_local import *
 except ImportError:
     pass
-
